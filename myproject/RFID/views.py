b8d2abb73536d71b73f2710a50a3025fe4b1170c
rihjeo <39438575+rihjeo@users.noreply.github.com>
2018-09-22 15:29:48 +0900

    view.py modify

--- myproject/RFID/views.py
+++ myproject/RFID/views.py
@@ -1,33 +1,41 @@
 from django.shortcuts import render
+from .models import RfidDB
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import RfidDB
 import json
 
 # Create your views here.
-
 @csrf_exempt
 def index(request):
     if request.method == "PUT":
-        rfidDB = RfidDB.objects.all()
-        context = {'rfidData' : rfidDB}
         try:
-            massage = str(request.read())
-            info = massage.split(",")
-            mname , mserial = info[0] , info[1]
-            data = RfidDB(name=mname, serial=mserial)
-            data.save()
-        except:
-            data = None
-    return HttpResponse(massage)
+            message = request.read()
+            message = message.decode()
+            info = message.split(" ")
+            mserial, mstatus = info[0],info[1]
+            rfidData = RfidDB.objects.get(serial = mserial)
+            rfidData.status = mstatus
+            rfidData.save()
+            # rfiddatas = RfidDB(serial=mserial,status=mstatus)
 
+        except:
+            rfiddatas = None
+        return HttpResponse("modify")
 def log(request):
     rfidDB = RfidDB.objects.all()
     str = ''
     for log in rfidDB:
-        str += "Name {} SerialNum {}<br>".format(log.name, log.serial)
+        str += "SerialNum {} Status {}<br>".format(log.serial, log.status)
     return HttpResponse(str)
 
+
 def check(request, serialNum):
-    num = serialNum
-    return HttpResponse(num)
+    rfidDB = RfidDB.objects.all()
+    ser = []
+    serialNum = int(serialNum)
+    for log in rfidDB:
+        ser.append(log.serial)
+    if serialNum in ser:
+        return HttpResponse("ok")
+    else:
+        return HttpResponse("no")
