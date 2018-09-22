a6eeca01e66abe1510891bc0da4e671d24c1ec25
seyong00545 <38447392+seyong00545@users.noreply.github.com>
2018-09-22 15:30:36 +0900

    Update IntegerField

--- myproject/RFID/models.py
+++ myproject/RFID/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class RfidDB(models.Model):
-    serial = models.IntegerField()
+    serial = models.IntegerField(default = 0)
     status = models.CharField(max_length=10)
 
     def __str__(self):
