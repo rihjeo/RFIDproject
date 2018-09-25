cc57c136bbed56f6da00c41ed9542b43ba28f100
seyong00545 <38447392+seyong00545@users.noreply.github.com>
2018-09-26 01:18:02 +0900

    Update models.py

--- myproject/RFID/models.py
+++ myproject/RFID/models.py
@@ -9,12 +9,9 @@ class Status(models.Model):
 
 class RfidDB(models.Model):
     serial = models.IntegerField(default = 0)
-<<<<<<< HEAD
     status = models.CharField(max_length = 10)
     time = models.CharField(max_length = 20, default = 0)
-=======
-    status = models.CharField(max_length=10)
->>>>>>> b977d037f2f5ca8f5f686e8c79e1ffe490139d88
+
 
     def __str__(self):
         return str(self.serial)
